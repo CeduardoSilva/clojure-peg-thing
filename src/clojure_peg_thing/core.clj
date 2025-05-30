52c2f72197d4fe35c5ee4b2a0ef129d5f0646164
Carlos Eduardo da Silva <cs.eduardo@icloud.com>
2025-05-30 19:30:01 -0300

    feat: adds traingular number functions

--- src/clojure_peg_thing/core.clj
+++ src/clojure_peg_thing/core.clj
@@ -1,7 +1,54 @@
 (ns clojure-peg-thing.core
+  (:require [clojure.set :as set])
   (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+
+(defn tri* 
+  "Generates lazy sequence of triangular numbers."
+  ([] (tri* 0 1))
+  ([sum n]
+    (let [new-sum (+ sum n)]
+      (cons new-sum (lazy-seq (tri* new-sum (inc n))))
+    )
+  )
+)
+
+(def tri (tri*))
+
+(defn triangular?
+  "Checks if number is triangular. e.g. 1, 3, 6,10. 15, etc"
+  [number]
+  (= number (last (take-while #(>= number %) tri)))
+)
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri))
+)
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 in row 1, positions 2 and 3 in row 2, etc."
+  [pos]
+  (inc (count (take-while #(> pos %) tri)))
+)
+
+(defn connect
+  "Form a mutual connection between two positions."
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+      (assoc-in new-board [p1 :connections p2] neighbor))
+      board
+      [[pos destination] [destination pos]]
+    )
+    board
+  ) 
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+

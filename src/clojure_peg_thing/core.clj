8118ac0bfd6211cfcaa2a5d8e6eeef5211937f2d
Carlos Eduardo da Silva <cs.eduardo@icloud.com>
2025-06-04 10:22:50 -0300

    feat: adds move peg functions

--- src/clojure_peg_thing/core.clj
+++ src/clojure_peg_thing/core.clj
@@ -47,6 +47,83 @@
   ) 
 )
 
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor (inc pos) destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination) board
+    )
+  )
+)
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ row pos)
+        destination (+ 1 row neighbor)
+       ]
+       (connect board max-pos pos neighbor destination)
+  )
+)
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ 1 row pos)
+        destination (+ 2 row neighbor)
+       ]
+       (connect board max-pos pos neighbor destination)
+  )
+)
+
+(defn add-pos
+  "Pegs the position and performs connections"
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-fn]
+        (connection-creation-fn new-board max-pos pos))
+        pegged-board
+        [connect-right connect-down-left connect-down-right]
+    )
+  )
+)
+
+(defn new-board 
+  "Creates a new board with the given number of row"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos] (add-pos board max-pos pos))
+      initial-board
+      (range 1 (inc max-pos))
+    )
+  )
+)
+
+(defn pegged?
+  "Does the position have a peg in it?"
+  [board pos]
+  (get-in board [pos :pegged])
+)
+
+(defn remove-peg
+  "Take the peg at given position out of the board"
+  [board pos]
+  (assoc-in board [pos :pegged false])
+)
+
+(defn place-peg
+  "Put a peg in the board at given position"
+  [board pos]
+  (assoc-in board [pos :pegged] true)
+)
+
+(defn move-peg
+  "Take peg out of p1 and place it in p2"
+  [board p1 p2]
+  (place-peg (remove-peg board p1) p2)
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
